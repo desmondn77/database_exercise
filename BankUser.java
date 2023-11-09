bdd7a81018d34711883139613237eb5a1c5f0bd1
torabihamed39 <torabihamed39@yahoo.com>
2023-11-10 00:29:53 +0330

    user information

---
+++ BankUser.java
@@ -0,0 +1,22 @@
+package com.example.springdatajpa1;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class BankUser {
+    @Id
+     Long id;
+    String firstname;
+    String lastname;
+
+
+
+
+}
